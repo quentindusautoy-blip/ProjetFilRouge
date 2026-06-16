df472c0415424c10eb809484ab51cf0347412a77
Quentin <quentin.dusautoy@gmail.com>
2026-06-16 18:03:03 +0200

    creation partie utilisateur

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -5,6 +5,9 @@ export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La galerie", "./pages/galerie.html"),
     new Route("/carte", "La galerie", "./pages/carte.html"),
+    new Route("/signin", "Connexion", "./pages/auth/signin.html"),
+    new Route("/signup", "Inscription", "./pages/auth/signup.html"),
+    new Route("/account", "Mon compte", "./pages/auth/account.html"),
 
     ];
 
