25c59f34009801835363a680aba49af0a61934da
Quentin <quentin.dusautoy@gmail.com>
2026-06-17 16:18:49 +0200

    Gestion de l'affichage des reservations

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -4,10 +4,11 @@ import Route from "./Route.js";
 export const allRoutes = [
     new Route("/", "Accueil", "./pages/home.html"),
     new Route("/galerie", "La galerie", "./pages/galerie.html"),
-    new Route("/carte", "La galerie", "./pages/carte.html"),
+    new Route("/carte", "La carte", "./pages/carte.html"),
     new Route("/signin", "Connexion", "./pages/auth/signin.html"),
     new Route("/signup", "Inscription", "./pages/auth/signup.html"),
     new Route("/account", "Mon compte", "./pages/auth/account.html"),
+    new Route("/allResa", "Toutes les reservations", "./pages/reservations/allResa.html"),
 
     ];
 
