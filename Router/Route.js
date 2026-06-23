09183ee043caed334f598aa34eb964d83b9004d6
Quentin <quentin.dusautoy@gmail.com>
2026-06-23 16:07:33 +0200

    afficher ou masquer en fonction du role de l'utilisateur

--- Router/Route.js
+++ Router/Route.js
@@ -1,10 +1,20 @@
 
 export default class Route {
 
-    constructor(url, title, pathHtml, pathJS = "") {
+    constructor(url, title, pathHtml,authorize = [], pathJS = "") {
       this.url = url;
       this.title = title;
       this.pathHtml = pathHtml;
       this.pathJS = pathJS;
+      this.authorize= authorize;
     }
 }
+
+
+/**
+  []-> Tout le monde peut y accéder
+  ["disconnected"]-> Réserver aux utilisateurs deconnecte
+  ["client"]-> Réserver aux utilisateurs avec le role client
+  ["admin"]-> Réserver aux utilisateurs avec le role admin
+  ["admin", "client"]-> Réserver aux utilisateurs avec le role admin ou client
+ */
--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -2,14 +2,14 @@ import Route from "./Route.js";
 
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "./pages/home.html"),
-    new Route("/galerie", "La galerie", "./pages/galerie.html"),
-    new Route("/carte", "La carte", "./pages/carte.html"),
-    new Route("/signin", "Connexion", "./pages/auth/signin.html","/js/auth/signin.js"),
-    new Route("/signup", "Inscription", "./pages/auth/signup.html", "/js/auth/signup.js"),
-    new Route("/account", "Mon compte", "./pages/auth/account.html"),
-    new Route("/allResa", "Toutes les reservations", "./pages/reservations/allResa.html"),
-    new Route("/reserver", "Réserver", "./pages/reservations/reserver.html"),
+    new Route("/", "Accueil", "./pages/home.html",[]),
+    new Route("/galerie", "La galerie", "./pages/galerie.html",[]),
+    new Route("/carte", "La carte", "./pages/carte.html",[]),
+    new Route("/signin", "Connexion", "./pages/auth/signin.html",["disconnected"],"/js/auth/signin.js"),
+    new Route("/signup", "Inscription", "./pages/auth/signup.html",["disconnected"], "/js/auth/signup.js"),
+    new Route("/account", "Mon compte", "./pages/auth/account.html",["admin", "client"]),
+    new Route("/allResa", "Toutes les reservations", "./pages/reservations/allResa.html",["client"]),
+    new Route("/reserver", "Réserver", "./pages/reservations/reserver.html",["client"]),
 
     ];
 
